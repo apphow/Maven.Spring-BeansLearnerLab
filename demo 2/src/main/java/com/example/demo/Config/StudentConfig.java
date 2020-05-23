0463181765affb733dd867aaf3c919ced6b61223
April Howard <ahoward@zipcodes-MBP-5.fios-router.home>
2020-05-23 02:34:50 -0400

    Created StudentConfig class

---
+++ demo 2/src/main/java/com/example/demo/Config/StudentConfig.java
@@ -0,0 +1,29 @@
+package com.example.demo.Config;
+
+import com.example.demo.models.Student;
+import com.example.demo.models.Students;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+
+@Configuration
+public class StudentConfig {
+    ArrayList<Student> students = new ArrayList<>();
+
+    @Bean(name = "currentStudents")
+    public Students currentStudents(){
+
+        students.add(new Student(8L, "April"));
+        students.add(new Student(10L, "Cameron"));
+        return new Students(students);
+    }
+
+    @Bean(name = "previousStudents")
+    public Students previousStudents(){
+        students.add(new Student(8L, "April"));
+        students.add(new Student(10L, "Cameron"));
+
+        return new Students(students);
+    }
+}
