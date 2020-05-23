cdb9082e1a71ce769cfead7b34eabd7bc27f1e9a
April Howard <ahoward@zipcodes-MBP-5.fios-router.home>
2020-05-23 02:47:11 -0400

    InstructorsConfig created

---
+++ demo 2/src/main/java/com/example/demo/Config/InstructorsConfig.java
@@ -0,0 +1,35 @@
+package com.example.demo.Config;
+
+import com.example.demo.models.Instructor;
+import com.example.demo.models.Instructors;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+
+@Configuration
+public class InstructorsConfig {
+    ArrayList<Instructor> instructors = new ArrayList<>();
+
+    @Bean(name = "tcUsaInstructors")
+    public Instructors tcUsaInstructors() {
+        instructors.add(new Instructor(10L, "Kris"));
+        instructors.add(new Instructor(8L, "Dolio"));
+        instructors.add(new Instructor(3L, "Chris"));
+        return new Instructors(instructors);
+    }
+
+    @Bean(name = "tcUKINstructors")
+    public Instructors tcUKInstructors() {
+        instructors.add(new Instructor(8L, "Dolio"));
+        instructors.add(new Instructor(3L, "Chris"));
+        return new Instructors(instructors);
+    }
+
+    @Bean(name = "instructors")
+    public Instructors instructors(){
+        instructors.add(new Instructor(8L, "Dolio"));
+        instructors.add(new Instructor(3L, "Chris"));
+        return new Instructors(instructors);
+    }
+}
