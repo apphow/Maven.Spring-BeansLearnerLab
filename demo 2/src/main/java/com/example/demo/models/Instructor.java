a14e546918ed8fbb49efad1ce1199f41fd0ed4e0
April Howard <ahoward@zipcodes-MBP-5.fios-router.home>
2020-05-23 01:31:09 -0400

    created Instructor class with teach method and lecture method that divides the numberOfHours evenly among learners

---
+++ demo 2/src/main/java/com/example/demo/models/Instructor.java
@@ -0,0 +1,26 @@
+package com.example.demo.models;
+
+public class Instructor extends Person implements Teacher {
+
+
+    public Instructor(Long id, String name) {
+        super(id, name);
+    }
+
+    @Override
+    public void teach(Learner learner, double numberOfHours) {
+        learner.learn(numberOfHours);
+
+    }
+
+    @Override
+    public void lecture(Iterable<? extends Learner> learners, double numberOfHours) {
+        int numberOfLearners = 0;
+        for(Learner learner : learners) {
+            numberOfLearners++;
+        }
+        for(Learner each : learners) {
+            each.learn(numberOfHours/numberOfLearners);
+        }
+    }
+}
