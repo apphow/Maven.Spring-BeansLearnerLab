6ac641add10f9e1c8c1c636939cfbcc5aaf083e7
April Howard <ahoward@zipcodes-MBP-5.fios-router.home>
2020-05-23 02:16:00 -0400

    created Classroom class

---
+++ demo 2/src/main/java/com/example/demo/models/Classroom.java
@@ -0,0 +1,32 @@
+package com.example.demo.models;
+
+public class Classroom {
+    Instructors instructors;
+    Students students;
+
+    public Classroom(Instructors instructors, Students students) {
+        this.instructors = instructors;
+        this.students = students;
+    }
+
+    public Instructors getInstructors() {
+        return instructors;
+    }
+
+    public void setInstructors(Instructors instructors) {
+        this.instructors = instructors;
+    }
+
+    public Students getStudents() {
+        return students;
+    }
+
+    public void setStudents(Students students) {
+        this.students = students;
+    }
+
+    public void hostLecture(Teacher teacher, double numberOfHours) {
+        teacher.lecture(students, numberOfHours);
+
+    }
+}
